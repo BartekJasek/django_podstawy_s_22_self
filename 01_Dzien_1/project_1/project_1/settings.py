46b645ab7aa91a40fc69271fa25ce08d4cc073b0
j.grynczewski <jgrynczewski@gmail.com>
2022-12-03 18:18:39 +0100

    day 1 widoki zadanie 2

--- 01_Dzien_1/project_1/project_1/settings.py
+++ 01_Dzien_1/project_1/project_1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'exercises_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'exercises_db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.0.1'
     }
 }
 
