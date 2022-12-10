e37895bad503445ed83353a5afaf03641cf2fa57
j.grynczewski <jgrynczewski@gmail.com>
2022-12-10 16:46:11 +0100

    relacje - zad 8

--- project/football/views.py
+++ project/football/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from football.models import Team
+
 # Create your views here.
+def league_table(request):
+
+    teams = Team.objects.order_by('-points')
+
+    return render(
+        request,
+        'league_table.html',
+        context={
+            'teams': teams,
+        }
+    )
