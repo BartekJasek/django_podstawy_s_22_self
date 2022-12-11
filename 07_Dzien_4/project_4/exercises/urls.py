b729edbac9141b367d3247f8280e8fc449dc8e95
j.grynczewski <jgrynczewski@gmail.com>
2022-12-11 12:32:29 +0100

    Cookie - zad 1 cd

--- 07_Dzien_4/project_4/exercises/urls.py
+++ 07_Dzien_4/project_4/exercises/urls.py
@@ -5,4 +5,6 @@ from exercises import views
 
 urlpatterns = [
     path('set-cookie/', views.set_cookie),
+    path('show-cookie/', views.show_cookie),
+    path('delete-cookie/', views.delete_cookie),
 ]
--- 07_Dzien_4/project_4/exercises/views.py
+++ 07_Dzien_4/project_4/exercises/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 # Cookies - zad 1
@@ -11,3 +11,24 @@ def set_cookie(request):
 
     return response
 
+
+def show_cookie(request):
+
+    user = request.COOKIES.get('User')
+    if not user:
+        return HttpResponse("Brak użytkownika")
+
+    return render(
+        request,
+        'show_cookie.html',
+        context={
+            'user': user,
+        }
+    )
+
+
+def delete_cookie(request):
+    response = HttpResponse("OK")
+    response.delete_cookie('User')
+
+    return response
